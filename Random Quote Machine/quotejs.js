e8f3739d614e29a0b2e826adc1d2a54a1e6dc946
David Dorr <shishkebab101@hotmail.com>
2016-06-27 15:43:23 +0300

    got random quote machine to...
    get quotes from an random quote api,
    tweet 'hello world' quote

--- Random Quote Machine/quotejs.js
+++ Random Quote Machine/quotejs.js
@@ -6,6 +6,26 @@
 
 $(function() {
 	$('#msg-btn').on('click', function() { // event handler when button clicked
-		$('#message-box').html("Here is your new message");
+		$.getJSON("http://api.icndb.com/jokes/random" , function(json) {
+
+			var html = "";
+
+			
+				html += '<blockquote><p id="inner_msg">';
+				html += json.value.joke;
+				html += "</p></blockquote>";
+			
+
+			$('#message-box').html(html);
+		});
 	});
+
+
 });
+
+
+// {"type":"success",
+// "value":{"id":422,
+// 		"joke":"Mr. T pities the fool. Chuck Norris rips the fool's head off.",
+// 		"categories":[]}
+// }
