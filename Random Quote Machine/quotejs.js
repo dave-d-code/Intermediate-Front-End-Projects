0f66e5701cbb47fd26cbe23eb689c1e0ca9a3f63
David Dorr <shishkebab101@hotmail.com>
2016-06-27 13:48:04 +0300

    Basic setup with jquery file
    and working button to change message in a
    message box.

---
+++ Random Quote Machine/quotejs.js
@@ -0,0 +1,11 @@
+
+
+// Jquery code to 
+// 1. connect with a random quote API...
+// 2. send API as a tweet on twitter. 
+
+$(function() {
+	$('#msg-btn').on('click', function() { // event handler when button clicked
+		$('#message-box').html("Here is your new message");
+	});
+});
