88400fb2611c8c20cd0eaf1fe7965da41c8c0418
David Dorr <shishkebab101@hotmail.com>
2016-07-02 14:23:58 +0300

    started the twicth tv app
    minor alteration to wikipedia app as well

---
+++ twitch_tv/twitch_js.js
@@ -0,0 +1,47 @@
+// need AJAX function to experiment with calls.
+
+var output;
+
+
+$(function() {
+
+	$('#hit').click(function() {
+		console.log('i heard hit');
+		searchTwitch();
+	});
+
+
+});
+
+function searchTwitch () {
+	
+	var http_string1 = "https://api.twitch.tv/kraken/streams/freecodecamp?callback=?";
+	var http_string2 = "https://api.twitch.tv/kraken/streams?game=StarCraft+II&callback=?";
+	var http_string3 = "https://api.twitch.tv/kraken/streams?channel=freecodecamp,sheevergaming,OgamingSC2&callback=?" // works, no info returned for 
+	var http_string4 = "https://api.twitch.tv/kraken/streams?game=StarCraft+II&limit=2&callback=?";
+	var http_string5 = "https://api.twitch.tv/kraken/streams?channel=freecodecamp,storbeck,terakilobyte&stream_type=playlist&callback=?"; // should be offline
+	var http_string6 = "https://api.twitch.tv/kraken/streams?channel=comster404&callback=?" // account closed...
+	var http_string7 = "https://api.twitch.tv/kraken/users/freecodecamp"; // user gives logos at least
+	var http_string8 = "https://api.twitch.tv/kraken/users/OgamingSC2";
+	var http_string9 = "https://api.twitch.tv/kraken/channel/freecodecamp";
+	var http_string10 = "https://api.twitch.tv/kraken/channels/freecodecamp"; // this might be it to test for account status
+	var http_string11 = "https://api.twitch.tv/kraken/channels/brunofin";
+	var http_string12 = "https://api.twitch.tv/kraken/channels?channel=freecodecamp";
+
+
+
+	$.ajax({
+		type : "GET",
+		url : http_string12,
+		async : false,
+		dataType : "json",
+		success : function(data) {
+			$('#outputView').html(JSON.stringify(data));
+			output = data;
+		},
+		error : function(error) {
+			$('#outputView').html('that account is fffff');
+		}
+	});
+
+}
